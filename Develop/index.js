74dd58b523ebf4bd84f4f6772a096a3f60a0761a
Andrew0502 <agcmarcus@gmail.com>
2020-09-23 19:08:53 -0400

    Typed out scafold of questions array.

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,44 @@
+var inquirer = require("inquirer");
+var fs = require('fs');
+
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "What is the description of your project.?"
+      },
+      {
+        type: "checkbox",
+        message: "What languages do you know?",
+        name: "stack",
+        choices: [
+          "HTML", 
+          "CSS", 
+          "JavaScript", 
+          "MySQL"
+        ]
+      },
+      {
+        type: "list",
+        message: "What is your preferred method of communication?",
+        name: "contact",
+        choices: [
+          "email",
+          "phone",
+          "telekinesis"
+        ]
+      }
+    ].then(function(data) { })
     
 
     // title, Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-];
+
 
 // function to write README file
 function writeToFile(fileName, data) {
