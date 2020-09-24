c06333da9d13596d0e29f3f8a83ae908c5f65397
Andrew0502 <agcmarcus@gmail.com>
2020-09-23 20:34:59 -0400

    built out return string interpolation in generateMarkdown.js.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,23 +1,41 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  let descriptionText = "";
+  let contentsText = "";
+  let installationText = "";
+  let usageText = "";
   let licenseText = "";
+  let contributionText = "";
+  let testsText = "";
+  let questionsText = "";
+  
+
   if(data.license ==="MIT"){
-    licenseText = "THis application is licensed under the MIT license..."
+    licenseText = "This application is licensed under the MIT license..."
   } else if(data.license === "GNU GPLv3") {
-    licenseText = "THis application is licensed under the GNU GPLv3 license..."
+    licenseText = "This application is licensed under the GNU GPLv3 license..."
   } else if(data.license === "ISC") {
-    licenseText = "THis application is licensed under the ISC license..."
+    licenseText = "This application is licensed under the ISC license..."
   } else if(data.license === "Apache 2.0") {
-    licenseText = "THis application is licensed under the Apache License 2.0 license..."
+    licenseText = "This application is licensed under the Apache License 2.0 license..."
 
   return `# ${data.title}
 ## ${data.description}
-##
-##
-##
-${data.license}
-MIT
-${licenseText}
+    ${descriptionText}
+## ${data.contents}
+    ${contentsText}
+## ${data.installation}
+    ${installationText}
+## ${data.usage}
+    ${usageText}
+## ${data.license}
+    ${licenseText}
+## ${data.contribution}
+    ${contributionText}  
+## ${data.tests}
+    ${testsText}
+## ${data.questions}
+    ${questionsText}
 `;
 }
 
